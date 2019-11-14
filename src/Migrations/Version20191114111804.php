8a4f691f8e674d16fede8e3c3f23fd65f6c04c47
TsankoBg <tsankohadzhiev@gmail.com>
2019-11-14 20:46:48 +0100

    migration

---
+++ src/Migrations/Version20191114111804.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191114111804 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, barcode VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, quantity DOUBLE PRECISION DEFAULT NULL, price NUMERIC(10, 4) NOT NULL, price_bought NUMERIC(10, 4) DEFAULT NULL, date_added DATETIME DEFAULT NULL, date_updated DATETIME DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_D34A04AD97AE0266 (barcode), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_general_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE product');
+    }
+}
