55ada4a11a186b2834962f6fe30fd6bdc052d00d
TsankoBg <tsankohadzhiev@gmail.com>
2020-03-07 17:15:16 +0200

    replaced ManagerRegistry in productrepo

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
  * @method Product|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class ProductRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Product::class);
     }
